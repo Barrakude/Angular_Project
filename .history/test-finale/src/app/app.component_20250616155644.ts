190855ae7671442ee21028fa4c09b8d171779a0f
Jacopo Fornaro <jacopofornaro@gmail.com>
2025-06-18 12:46:59 +0200

    commit for history folder

---
+++ .history/test-finale/src/app/app.component_20250616155644.ts
@@ -0,0 +1,14 @@
+import { TitleCasePipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+@Component({
+  selector: 'tmg-root',
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'task-management-fe';
+}
---
+++ .history/test-finale/src/app/features/task-list/task-list.component_20250618110447.ts
@@ -0,0 +1,33 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { TaskListService } from '../../shared/services/task-list.service';
+import { ITask } from '../../shared/interfaces/itask';
+import { take, tap } from 'rxjs';
+
+@Component({
+  selector: 'tmg-task-list',
+  imports: [],
+  templateUrl: './task-list.component.html',
+  styleUrl: './task-list.component.scss'
+})
+export class TaskListComponent implements OnInit {
+
+  private readonly _taskService:TaskListService = inject(TaskListService);
+
+  tasks:ITask[]=[];
+
+
+ngOnInit(): void {
+  this.getTask();
+}
+
+
+  getTask():void{
+    this._taskService.getTasks().pipe(
+      take(1),
+      tap((data)=>{
+        this.tasks = data;
+      })
+    ).subscribe()
+  }
+
+}
